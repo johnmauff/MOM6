edd6d3c83c0fb38e301adfd2f005c75cf1f182b9
Marshall Ward <marshall.ward@noaa.gov>
2025-12-05 17:02:02 -0500

    (+) Decouple FMS infra from framework
    This patch undoes a coupling of the FMS infra layer to the MOM6
    framework code.
    
    In the current FMS infra layers, the `get_extern_field_info()` and
    `init_extern_field()` functions require content defined in
    `src/framework`.   This prevents the development of new
    independent infra layers, which much also depend on infra-agnostic
    content.
    
    In particular, the FMS2 implementation of `get_extern_field_axes()`
    relies exclusively on the framework function, `get_var_axes_info()`.
    
    Both infras also return the `axes_info` type, a MOM-specific
    framework-level descriptor, rather than the infra `axistype`.
    
    This patch resolves these inconsistencies.
    
    * `axis_info` no longer appears at infra-level.  All relevant functions
      now reference `axistype`.
    
    * `src/framework/MOM_io.F90` now provide functions for translating
      `axistype` to `axis_info`.
    
    Some specific changes are summarized below.
    
    * `get_external_field_info` is now a framework-level function of
      `MOM_interpolate.F90` , using infra-level implementations of
      `get_extern_field_(size|axes|missing)`.  Each is now explicitly
      defined at the infra-level.
    
    * The FMS2 `get_external_field_axes` is now an entirely new function,
      and is largely a duplicate of `get_var_axes_info()`.  The major
      difference is that it returns a list of `axistype`.  It also replaces
      the fixed x-y-z fetch with a slightly more generic list of axes.
    
      (It still requires at least three dimensions, however.)
    
    * `set_axis_data` is only used internally by the FMS2 infra.  It is
      included in FMS1 but raises an nonimplementation error.
    
    There is one minor API change.
    
    * The `name` argument was added to `get_axis_data`.  It is now the
      second argument, to match the style of existing functions, and size
      was moved to the third argument.
    
    Other minor framework references have been removed.
    
    * `MOM_error` and `FATAL` now refernce their `MOM_error_infra`
      equivalents.
    
    * `lowercase`, which was previously only defined in FMS1, has been added
      to the FMS2 infra.  Note that this is a duplication of the function in
      `src/framework/MOM_string_functions.F90`.

--- config_src/infra/TIM/MOM_io_infra.F90
+++ config_src/infra/TIM/MOM_io_infra.F90
@@ -46,7 +46,8 @@ integer, parameter :: MULTIPLE = 401
 public :: open_file, open_ASCII_file, file_is_open, close_file, flush_file, file_exists
 public :: get_file_info, get_file_fields, get_file_times, get_filename_suffix
 public :: read_field, read_vector, write_metadata, write_field
-public :: field_exists, get_field_atts, get_field_size, get_axis_data, read_field_chksum
+public :: field_exists, get_field_atts, get_field_size, read_field_chksum
+public :: get_axis_data, set_axis_data
 public :: io_infra_init, io_infra_end, MOM_namelist_file, check_namelist_error, write_version
 public :: stdout_if_root
 ! These types act as containers for information about files, fields and axes, respectively,
@@ -716,19 +717,46 @@ end function find_index
 
 
 !> Extracts and returns the axis data stored in an axistype.
-subroutine get_axis_data( axis, dat )
-  type(axistype),     intent(in)  :: axis !< An axis type
-  real, dimension(:), intent(out) :: dat  !< The data in the axis variable
+subroutine get_axis_data(axis, axis_name, axis_data)
+  type(axistype), intent(in) :: axis   !< Infra axis
+  character(len=256), intent(out) :: axis_name   !< Axis name
+  real, dimension(:), intent(out) :: axis_data   !< Axis points
 
   integer :: i
 
-  ! This routine might not be needed for MOM6.
   if (allocated(axis%ax_data)) then
-    if (size(axis%ax_data) > size(dat)) call MOM_err(FATAL, &
-      "get_axis_data called with too small of an output data array for "//trim(axis%name))
-    do i=1,size(axis%ax_data) ; dat(i) = axis%ax_data(i) ; enddo
+    if (size(axis%ax_data) > size(axis_data)) &
+      call MOM_error(FATAL, "get_axis_data called with too small of an " &
+          // "output data array for " // trim(axis%name) // ".")
+    do i=1,size(axis%ax_data)
+      axis_data(i) = axis%ax_data(i)
+    enddo
   endif
+
+  axis_name = axis%name
+
 end subroutine get_axis_data
+
+!> Return a new axistype based on axis specs
+subroutine set_axis_data(axis, axis_name, axis_data)
+  type(axistype), intent(inout) :: axis
+    !< Target axis
+  character(len=256), intent(in) :: axis_name
+    !< Target axis name
+  real, intent(in) :: axis_data(:)
+    !< Target axis values
+
+  axis%name = axis_name
+
+  if (allocated(axis%ax_data)) deallocate(axis%ax_data)
+  allocate(axis%ax_data(size(axis_data)))
+
+  axis%ax_data(:) = axis_data(:)
+
+  ! NOTE: We do not yet consider domain-decomposed axes.
+  axis%domain_decomposed = .false.
+end subroutine set_axis_data
+
 
 !> This routine uses the fms_io subroutine read_data to read a scalar named
 !! "fieldname" from a single or domain-decomposed file "filename".
